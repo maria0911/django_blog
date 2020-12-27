6f5b242921439b5b94a2cc603c4289a5063d9b95
maria <maria@localhost>
2020-12-27 15:59:25 +0700

    bootstrap nav bar and display dummy data

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
+posts = [
+    {
+        'author': 'Maria',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': '27 December 2020'},
+    {
+        'author': 'Jane',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': '28 December 2020'}
+    ]
+
 def home(request):
-    return HttpResponse('<h1>Home Page</h1>')
+    context = {
+        'posts': posts
+        }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>About Page</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
 
