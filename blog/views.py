97ba611e049085fb67ffbb706a18cd3ca3f75a04
maria <maria@localhost>
2020-12-31 03:07:58 +0700

    delete dummy data and register database model into admin

--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
-posts = [
-    {
-        'author': 'Maria',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': '27 December 2020'},
-    {
-        'author': 'Jane',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': '28 December 2020'}
-    ]
-
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
         }
     return render(request, 'blog/home.html', context)
 
