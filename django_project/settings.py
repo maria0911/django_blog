ccfaab4de96e7dbeb68bdbcee2fd0bb8643e6a89
maria <maria@localhost>
2020-12-30 11:35:57 +0700

    installed mysql and created database model

--- django_project/settings.py
+++ django_project/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'django_database',
+        'USER': 'maria',
+        'PASSWORD': 'masha3919',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
