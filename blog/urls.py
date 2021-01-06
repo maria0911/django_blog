567432d12bb7127108eafa4b34ae85e9b27ae584
maria <maria@localhost>
2021-01-07 03:37:43 +0700

    added create new post form and reverse to the post detail

--- blog/urls.py
+++ blog/urls.py
@@ -4,12 +4,13 @@ Created on Dec 24, 2020
 @author: maria
 '''
 from django.urls import path
-from .views import PostListView, PostDetailView
+from .views import PostListView, PostDetailView, PostCreateView
 from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('about/', views.about, name='blog-about'),
 ]
 
