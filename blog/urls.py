4db2eda787b3e106ce73f6bf7de30cc44db237de
maria <maria@localhost>
2021-01-07 03:19:32 +0700

    added class listview and detailview

--- blog/urls.py
+++ blog/urls.py
@@ -4,9 +4,12 @@ Created on Dec 24, 2020
 @author: maria
 '''
 from django.urls import path
+from .views import PostListView, PostDetailView
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('about/', views.about, name='blog-about'),
 ]
+
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 def home(request):
@@ -7,6 +8,17 @@ def home(request):
         'posts': Post.objects.all()
         }
     return render(request, 'blog/home.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html' #<app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    
+class PostDetailView(DetailView):
+    model = Post
+    #template_name = 'blog/post_detail.html'
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
