a71a53d355929214246db99f34efbb18c0dcd7d2
Maria Ugnich <maria@Marias-MacBook-Air.local>
2021-01-12 02:20:48 +0700

    set Debug Value

--- django_project/settings.py
+++ django_project/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 #SECRET_KEY = ')wy6#3=a+#%9$4imj)l%(yu2v-f*4+rl+=#al328u906$c^-vd'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['mariablog.herokuapp.com']
 
